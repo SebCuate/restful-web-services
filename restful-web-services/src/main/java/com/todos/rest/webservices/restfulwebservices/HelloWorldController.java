5a1938187cfe04a6e39945f4c3c7eb6d016f3607
Sebastian Cuatepotzo <elcuato@localhost.localdomain>
2019-09-19 23:14:10 -0500

    Rest services for todos

---
+++ restful-web-services/src/main/java/com/todos/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.todos.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	//GET
+	//URI - /hello-world
+	//method - "Hello World"
+//	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		
+		return "Hello World";
+	}
+	
+	//Hello world with name
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		
+		return new HelloWorldBean("Hello World");
+	}
+	
+}
